7ca4ef51c2c1135912dafa624225431f7f36ef66
dwkns <dwkns@me.com>
2013-11-25 17:34:26 +0000

    Added submission model, submission controller, and conditional submit photo button

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131125151923) do
+ActiveRecord::Schema.define(version: 20131125164723) do
+
+  create_table "submissions", force: true do |t|
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
