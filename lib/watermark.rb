59f1684c4bbd1b80a177b9b3d613ad3a4c956e44
dwkns <dwkns@me.com>
2013-11-26 20:28:09 +0000

    image manipulation sorted

--- lib/watermark.rb
+++ lib/watermark.rb
@@ -1,99 +1,37 @@
-
-
-
-
-  class Watermark < Processor
-    # Handles watermarking of images that are uploaded.
-    attr_accessor :current_geometry, :target_geometry, :format, :whiny, :convert_options, :watermark_path, :watermark_offset, :overlay, :position
- 
+module Paperclip
+  class Polarize < Processor
     def initialize file, options = {}, attachment = nil
-       super
-       p "------------Watermark options--------------------"
-       p "-------------------------------------------"
-       p "-------------------------------------------"
-       p "options : #{options}"
-       p "-------------------------------------------"
-       p "-------------------------------------------"
-       p "-------------------------------------------"
-       
-       geometry          = options[:geometry]
-       @file             = file
-       @crop             = geometry[-1,1] == '#'
-       @target_geometry  = Geometry.parse geometry
-       @current_geometry = Geometry.from_file @file
-       @convert_options  = options[:convert_options]
-       @whiny            = options[:whiny].nil? ? true : options[:whiny]
-       @format           = options[:format]
-       @watermark_path   = options[:watermark_path]
-       @position         = options[:position].nil? ? "SouthEast" : options[:position]
-       @watermark_offset = options[:watermark_offset]
-       @overlay          = options[:overlay].nil? ? true : false
-       @current_format   = File.extname(@file.path)
-       @basename         = File.basename(@file.path, @current_format)
-       
-       
+      super
+      @file           = file
+      @attachment     = attachment
+      @is_polarized   = options[:is_polarized]
+      @current_format = File.extname(@file.path) 
+      @format         = options[:format]
+      @basename       = File.basename(@file.path, @current_format)
+    end
 
-       # raise "options :  #{@watermark_path}"
-       # raise "options :  #{options}"
-     end
- 
-     # TODO: extend watermark
- 
-     # Returns true if the +target_geometry+ is meant to crop.
-      def crop?
-        @crop
-      end
- 
-      # Returns true if the image is meant to make use of additional convert options.
-      def convert_options?
-        not @convert_options.blank?
-      end
- 
-      # Performs the conversion of the +file+ into a watermark. Returns the Tempfile
-      # that contains the new image.
-      def make
-        dst = Tempfile.new([@basename, @format].compact.join("."))
-        dst.binmode
- 
-        if watermark_path
-          command = "composite"
-          params = %W[-gravity #{@position}]
-          params += %W[-geometry '#{@watermark_offset}'] if @watermark_offset
-          params += %W['#{watermark_path}' '#{fromfile}']
-          params += transformation_command
-          params << "'#{tofile(dst)}'"
-        else
-          command = "convert"
-          params = ["'#{fromfile}'"]
-          params += transformation_command
-          params << "'#{tofile(dst)}'"
-          # super
-        end
+    def make   
+      temp_file = Tempfile.new([@basename, @format])
+      temp_file.binmode
 
-          
-        # begin
-          Paperclip.run(command, params.join(' '))
-        # rescue ArgumentError, Cocaine::CommandLineError
-          # raise PaperclipError, "There was an error processing the watermark for #{@basename}" if @whiny
-        # end
- 
-        dst
+      if @is_polarized
+
+        run_string =  "convert #{fromfile} -thumbnail 300x400  -bordercolor white -background black  +polaroid  #{tofile(temp_file)}"    
+        
+        # raise run_string
+        Paperclip.run(run_string)
       end
- 
-      def fromfile
-        File.expand_path(@file.path)
-      end
- 
-      def tofile(destination)
-        File.expand_path(destination.path)
-      end
- 
-      def transformation_command
-        scale, crop = @current_geometry.transformation_to(@target_geometry, crop?)
-        trans = %W[-resize '#{scale}']
-        trans += %W[-crop '#{crop}' +repage] if crop
-        trans << convert_options if convert_options?
-        trans
-      end
+  
+      temp_file
+    end
+
+    def fromfile
+      File.expand_path(@file.path)
+    end
+
+    def tofile(destination)
+
+      File.expand_path(destination.path)
+    end
   end
 end
