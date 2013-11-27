50ddcf376d5fdf159db0347ba4fd1b68111203ed
Nadia Odunayo <nodunayo@gmail.com>
2013-11-27 16:16:41 +0000

    Added tests for validations

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Submission do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:submission) {Submission.new photo: Rails.root.join("app/assets/images/dogs.png").open, description: "Dogs"}
+  it { should belong_to(:user) }
+
+  context 'it is valid when' do
+    it 'has a description of at least 3 characters and an image attached' do
+      expect(submission).to be_valid
+    end
+  end
+
+  context 'it is not valid when' do
+    it 'has a description fewer than 3 characters' do
+      submission.description = "Be"
+      expect(submission).not_to be_valid
+    end
+
+    it 'has no image attached' do
+      submission.photo = nil
+      expect(submission).not_to be_valid
+    end
+  end
+
 end
