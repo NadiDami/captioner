c9a6ef9ef0395c7343a86ca06eb7c34b16130a0d
Nadia Odunayo <nodunayo@gmail.com>
2013-11-27 13:16:09 +0000

    Added bootstrap

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,6 @@
 class Submission < ActiveRecord::Base
 
-    has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
+    has_attached_file :photo, styles: { medium: "250x250>", thumb: "100x100>" }
 
     belongs_to :user
 end
