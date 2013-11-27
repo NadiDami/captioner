6739d75c623851c47eecf027ff8ea43348227bf5
Nadia Odunayo <nodunayo@gmail.com>
2013-11-27 16:16:41 +0000

    Added tests for validations

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,4 +3,7 @@ class Submission < ActiveRecord::Base
     has_attached_file :photo, styles: { medium: "250x250>", thumb: "100x100>" }
 
     belongs_to :user
+
+    validates :description, length: { minimum: 3}
+    validates :photo, attachment_presence: { message: 'Make sure to attach a photo!'}
 end
