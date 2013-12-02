84620ad6d60737828f131af64aca026cddae28b7
dwkns <dwkns@me.com>
2013-12-02 19:23:59 +0000

    Captioning is working

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,7 +1,16 @@
 class Submission < ActiveRecord::Base
     # before_post_process :before_post_process
 
-    has_attached_file :photo, styles: { medium: "200x200^"}
+    # has_attached_file :photo, styles: { medium: "200x200^"}
+
+    has_attached_file :photo,
+                  processors: [:polarize],
+                  styles: {
+                    polarized: {
+                      format: '.png',
+                      is_polarized: true
+                    }
+                  } 
 
 
     belongs_to :user
