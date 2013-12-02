367ed913c947ccad61f9bd19973f2f5b0e98d415
dwkns <dwkns@me.com>
2013-12-02 18:11:44 +0000

    fuck load of changes

--- app/models/caption.rb
+++ app/models/caption.rb
@@ -1,12 +1,22 @@
 class Caption < ActiveRecord::Base
   has_attached_file :captioned_photo,
                   processors: [:captionbitch],
-                  styles: {
-                    polarized: {
-                      format: '.png',
-                      is_polarized: true
+                  styles: -> (attachment) {
+                      {
+                      comic: {
+                        format: '.png',
+                        comic: true,
+                        the_text: attachment.instance.text,
+                        original_image: attachment.uploaded_file,
+                        mask: Rails.root.join('public/mask.png')
+                      }
                     }
                   } 
 
   belongs_to :submission
 end
+
+
+
+
+
